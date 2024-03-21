230ca24d884dc24caa85e35bd20694e491e5f477
lee-dias <leedias.dev@gmail.com>
2024-03-21 16:25:37 +0000

    variaveis crono1 e crono2 com crono1 a ser iniciada

---
+++ Chronos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+            crono1.Start();
+        }   
+    }
+}
