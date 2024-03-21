c5958dc67f24ad8ce4cd090d91fd07b31771a1fb
lee-dias <leedias.dev@gmail.com>
2024-03-21 16:45:14 +0000

    mete em string o tempo precorrido dos cronos e mostra no ecra

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -19,7 +19,19 @@ namespace Chronos
             TimeSpan crono1ts = crono1.Elapsed;
             TimeSpan crono2ts = crono2.Elapsed;
 
+            string crono1elapsedTime =
+            String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            crono1ts.Hours, crono1ts.Minutes, crono1ts.Seconds,
+            crono1ts.Milliseconds / 10);
 
+            string crono2elapsedTime = 
+            String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            crono2ts.Hours, crono2ts.Minutes, crono2ts.Seconds,
+            crono2ts.Milliseconds / 10);
+
+            Console.WriteLine("RunTime crono1: " + crono1elapsedTime);
+            Console.WriteLine("RunTime crono2: " + crono2elapsedTime);
+            
         }   
     }
 }
