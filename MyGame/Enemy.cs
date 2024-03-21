87638e1476dee108a94c1f8ee5eda1f1a4fc817b
lee-dias <leedias.dev@gmail.com>
2024-03-21 20:04:10 +0000

    add dos dois metodos do Enemy

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -12,6 +12,19 @@ namespace MyGame
 
         } 
 
+        public string GetName(){
+            return name;
+        }
+
+        public void TakeDamage(float damage){
+            shield -= damage;
+            if (shield<0){
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= damageStillToInflict;
+                if(health<0)health = 0;
+            }
+        }
     }
 
 
