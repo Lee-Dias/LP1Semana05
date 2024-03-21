0b0f487c04152c24c64025c23e88b8950c2efbe5
lee-dias <leedias.dev@gmail.com>
2024-03-21 20:15:14 +0000

    add dos metodos gethealth e getshield e setname e o contrutuor usa o set name

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -3,10 +3,10 @@ namespace MyGame
     public class Enemy
     {
         private string name;
-        private float health;
-        private float shield;
-        public Enemy(string name){
-            this.name = name;
+        private float health = 100;
+        private float shield = 0;
+        public Enemy(){
+            this.name = SetName();
             health = 100;
             shield = 0;
 
@@ -25,6 +25,20 @@ namespace MyGame
                 if(health<0)health = 0;
             }
         }
+        public float GetHealth(){
+            return health += 50;
+        }
+        
+        public float GetShield(){
+            return shield += 50;
+        }
+        public string SetName(){
+            const int MaxLength = 8;
+            if (name.Length > MaxLength)
+            name = name.Substring(0, MaxLength);
+            return name;
+        }
+
     }
 
 
