854deebc0b56585afd871335c7b72f0af038b34d
lee-dias <leedias.dev@gmail.com>
2024-03-21 19:33:40 +0000

    project gets the seed and makes random with the nº given

--- RanDice/Program.cs
+++ RanDice/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-
 namespace RanDice
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int sum = 0;
+            int n = int.Parse(args[0]);
+            int s = int.Parse(args[1]);
+            Random rand = new Random(s);
+            for (int i = 0; i<n; i++){
+                int diceValue = rand.Next(1, 7);
+                sum += diceValue;
+            }
+            
         }
     }
 }
