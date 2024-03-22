8ff6e706e7c64fe4b7cae9ad9d26d879e85b14a8
lee-dias <leedias.dev@gmail.com>
2024-03-22 14:57:57 +0000

    add dos gets todos pedidos

--- MyGameFriday/GameLevel.cs
+++ MyGameFriday/GameLevel.cs
@@ -10,6 +10,8 @@ namespace MyGame
     {
         private Difficulty diff;
         private Enemy[] numderooms = new Enemy[1];
+        private int enemyam = 0;
+        
 
         
         public GameLevel(int rooms, Difficulty dif){
@@ -19,6 +21,27 @@ namespace MyGame
         }
         public void SetEnemyInRoom(int rn, Enemy name){
             numderooms[rn] = name;
+            enemyam += 1;
+        } 
+        public string GetDifficulty(){
+            string d = "";
+            if(diff == Difficulty.Easy){
+                d = "Easy";
+            }else if(diff == Difficulty.Medium){
+                d = "Medium";
+            }else{
+                d = "Hard";
+            }
+            return d;
+        } 
+        public int GetNumRooms(){
+            return numderooms.Length;
+        } 
+        public int GetNumEnemies(){
+            return enemyam;
+        } 
+        public void PrintEnemies(){
+            Console.WriteLine("has enemies");
         } 
 
     }
